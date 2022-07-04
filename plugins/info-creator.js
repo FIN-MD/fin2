38106a805b00ff467fe6d63df19e6a0c133c9911
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2022-07-04 15:28:42 +0800

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ DaniOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ DaniOffcོ_1ঔৣ꧂
-item1.TEL;waid=6281251549899:6281251549899@s.whatsapp.net
+FN: FIN
+item.ORG: FIN
+item1.TEL;waid=6282261065144:6282261065144@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
-item2.EMAIL;type=INTERNET: kecedani257@gmail.com
+item2.EMAIL;type=INTERNET: udrehan8@gmail.com
 item2.X-ABLabel:📧Email
 item3.ADR:;;🎍KANDANGAN;;;;
-item3.X-ABADR:ac
+item3.X-ABLabel:ac
 item3.X-ABLabel:asal kota
-item4.URL:https://github.com/Bot-Mdnew
+item4.URL:https://github.com/Raya2399
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
